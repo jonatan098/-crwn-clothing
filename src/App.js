020f478278e0b6e132970818833f926ee35ae930
jonatan custodio <jonatanpcustodio@gmail.com>
2022-04-17 14:28:27 -0300

    🎉 my first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CategoriesComponent from "./components/categories-component/categories-component"
+
+import "./categories.styles.scss"
+
+const App = () => {
+  return (
+    <CategoriesComponent />
+  );
+};
+
+export default App;
